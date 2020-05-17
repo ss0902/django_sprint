e1a1e1885f56d5f4deeb8e38cbdb7f2d0b24c395
Shogo92 <shogo.gyomu@gmail.com>
2020-05-17 18:28:31 +0900

    Signup system

--- cms/views.py
+++ cms/views.py
@@ -1,11 +1,16 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login
 from django.contrib.auth.views import (
     LoginView, LogoutView,
 )
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import (
+    CreateView,
+)
 
 from .forms import (
-    LoginForm,
+    LoginForm, UserCreateForm,
 )
 
 UserModel = get_user_model()
@@ -22,3 +27,15 @@ class Login(LoginView):
 
 class Logout(LogoutView):
     pass
+
+
+class UserCreate(CreateView):
+    form_class = UserCreateForm
+    template_name = 'cms/signup.html'
+    success_url = reverse_lazy('cms:top')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        self.object = user
+        return HttpResponseRedirect(self.get_success_url())
